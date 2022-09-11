9c0442978b260209c46f098d6f2c1533ef269acd
TanNew2k <84030084+TanNew2k@users.noreply.github.com>
2022-09-11 22:25:01 +0700

    Update ApiKey

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,7 +12,7 @@ class Program
     static void Main()
     {
         const string clientID = "";
-        const string clientSecret = "f";
+        const string clientSecret = "";
 
         // Generates code verifier value.
         string codeVerifier = RandomDataBase64Url(32);
