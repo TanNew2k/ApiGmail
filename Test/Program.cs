444ea3d65228dcc7ccf4ea47b28d593978852017
TanNew2k <84030084+TanNew2k@users.noreply.github.com>
2022-09-11 22:21:23 +0700

    Update Key

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,8 +11,8 @@ class Program
 {
     static void Main()
     {
-        const string clientID = "";
-        const string clientSecret = "";
+        const string clientID = "vfvdfv";
+        const string clientSecret = "fdvdfvdfvdfv";
 
         // Generates code verifier value.
         string codeVerifier = RandomDataBase64Url(32);
